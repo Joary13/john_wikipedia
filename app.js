9fe741d90bde38496d08489ed66452e46f98b112
Joary13 <jolea13008@gmail.com>
2023-11-11 18:33:27 +0100

    wikipedia project finished

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const url =
-  'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=searchValue';
+  'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=';
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.form-input');
 const results = document.querySelector('.results');
 
-const fetchPages = async (data) => {
-  console.log(data);
+const renderResults = (data) => {
+  const html = data
+    .map(({ snippet, title, pageid }) => {
+      return `<a href="http://en.wikipedia.org/?curid=${pageid}" target="_blank">
+            <h4>${title}</h4>
+            <p>
+              ${snippet}
+            </p>
+          </a>`;
+    })
+    .join('');
+  results.innerHTML = `<div class="articles">${html}</div>`;
+};
+
+const fetchPages = async (value) => {
+  results.innerHTML = "<div class='loading'></div>";
+  try {
+    const urlcomplete = `${url}${value}`;
+    console.log(urlcomplete);
+    const response = await fetch(urlcomplete);
+    if (response.ok === false) throw new Error('url problem');
+    const data = await response.json();
+    const results = data.query.search;
+    if (results.length < 1) {
+      results.innerHTML = `<div class='error'>No matching result please try again</div>`;
+      return;
+    }
+    renderResults(results);
+  } catch (err) {
+    results.innerHTML = `<div class='error'>il y a une erreur: ${err}</div>`;
+  }
 };
 form.addEventListener('submit', (e) => {
   e.preventDefault();
